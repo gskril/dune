76dbd88771a4eae07a8edf0c2349f8b39b7b8a26
Greg Skriloff <35093316+gskril@users.noreply.github.com>
2025-06-18 01:52:05 -0400

    Add test with live data response

--- src/test.ts
+++ src/test.ts
@@ -1,3 +1,4 @@
+import 'dotenv/config'
 import assert from 'node:assert'
 import { test } from 'node:test'
 
@@ -7,4 +8,29 @@ test('should throw an error if no API key is provided', () => {
   assert.throws(() => {
     new Dune('')
   })
+})
+
+test('should return data from a query', async () => {
+  type DuneData = {
+    rank: number
+    username: string
+    followers: number
+  }
+
+  const dune = new Dune(process.env.DUNE_API_KEY)
+  const res = await dune.results<DuneData>(3224138) // Top 100 Farcaster users by followers
+
+  // We're expecting "dwr.eth" and "vitalik.eth" to be in the array, each with >400k followers
+  const targetUsernames = ['dwr.eth', 'vitalik.eth']
+  const targetUsers = res.data?.result?.rows.filter((row) =>
+    targetUsernames.includes(row.username)
+  )
+
+  assert.strictEqual(targetUsers?.length, 2)
+  for (const user of targetUsers) {
+    assert.ok(
+      user.followers > 400_000,
+      `Expected ${user.username} to have >400k followers`
+    )
+  }
 })
